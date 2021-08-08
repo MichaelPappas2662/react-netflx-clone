88a8c1475b44fe77a1e079b4802f5d557664c764
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 18:10:53 +1000

    fix: fixing the hashing of password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,22 +3,21 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 
 //REGISTER
-router.post("/register", async (req,res) => {
-    const newUser = new User({
-        username:req.body.username,
-        email:req.body.email,
-        password:CryptoJS.AES.encrypt(
-            req.body.password,
-            process.env.SECRET_KEY
-          ).toString(),
-    });
-
-    try {
-        const user = await newUser.save();
-        res.status(201).json(user);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
