83158b34577cb92c95bfa6cefd2db41108e57600
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 18:27:35 +1000

    feat: creating a jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -32,7 +33,11 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong password or username!");
 
-    
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
 
     const { password, ...info } = user._doc;
 
