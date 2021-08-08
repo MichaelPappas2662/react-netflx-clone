069dee7608ae4dfe808c027616975326edc5ca91
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 19:35:24 +1000

    feat: renderring movies routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const dotevnv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 dotevnv.config();
 
@@ -22,6 +23,7 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 
 app.listen(8800, ()=> {
