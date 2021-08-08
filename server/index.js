81d96e1d1949b6edda5216783fe3980544b4415b
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 17:57:27 +1000

    fix: adding createIndex mongooee

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("DB Connection Successful"))
   .catch((err) => {
