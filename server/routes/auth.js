ae0cafeea149ec9bea692f2f75a11ffa9309332e
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 17:53:12 +1000

    feat: adding more code on user auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,4 +2,12 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 //REGISTER
-router.post("/register",)
+router.post("/register",(req,res) => {
+    const newUser = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+    });
+
+    const user = newUser.save();
+})
