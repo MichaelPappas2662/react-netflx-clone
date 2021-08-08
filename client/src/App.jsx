b5c376e7689ed1436f9ef8c1652a5dc9b4578d67
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 17:09:49 +1000

    test: tessting the pages are rendering properly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss"
 import Home from './pages/home/Home';
+import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
 
 const  App=()  =>{
   return (
-    <Watch />
+    <Login />
   )
 }
 
