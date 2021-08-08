d3df458e3f2dd33f6625c2297b7f3536e323fca5
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 16:50:04 +1000

    feat: rendering the list items

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,19 @@
-import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
 import "./home.scss";
+import List from "../../components/list/List";
 
-function Home() {
-    return (
-        <div className="home">
-        <Navbar/>
-        <Featured/>
-        </div>
-    )
-}
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
