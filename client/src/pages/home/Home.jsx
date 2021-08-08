b1d87d0287f7dafc330b02fe56aeb0fd0706948c
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 16:41:30 +1000

    feat: rendering

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 
 function Home() {
     return (
+        <div className="home">
         <Navbar/>
+        <Featured/>
+        </div>
     )
 }
 
