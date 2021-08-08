72a9dece9adf21793102df0e2c61aa758168eb60
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 16:50:27 +1000

    feat: rendering the list

---
+++ client/src/components/list/List.jsx
@@ -0,0 +1,44 @@
+import {
+    ArrowBackIosOutlined,
+    ArrowForwardIosOutlined,
+  } from "@material-ui/icons";
+  import { useRef, useState } from "react";
+  import "./list.scss";
+  
+  export default function List() {
+    const [isMoved, setIsMoved] = useState(false);
+    const [slideNumber, setSlideNumber] = useState(0);
+  
+    const listRef = useRef();
+  
+    const handleClick = (direction) => {
+      setIsMoved(true);
+      let distance = listRef.current.getBoundingClientRect().x - 50;
+      if (direction === "left" && slideNumber > 0) {
+        setSlideNumber(slideNumber - 1);
+        listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      }
+      if (direction === "right" && slideNumber < 5) {
+        setSlideNumber(slideNumber + 1);
+        listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      }
+    };
+    return (
+      <div className="list">
+        <span className="listTitle">Continue to watch</span>
+        <div className="wrapper">
+          <ArrowBackIosOutlined
+            className="sliderArrow left"
+            onClick={() => handleClick("left")}
+            style={{ display: !isMoved && "none" }}
+          />
+          <div className="container" ref={listRef}>
+          </div>
+          <ArrowForwardIosOutlined
+            className="sliderArrow right"
+            onClick={() => handleClick("right")}
+          />
+        </div>
+      </div>
+    );
+  }
