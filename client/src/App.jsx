d63ef6ce23a388f0a169cf433075c0af56d9f923
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 17:03:32 +1000

    feat: rendering the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.scss"
-import Home from './pages/home/Home'
+import Home from './pages/home/Home';
+import Watch from "./pages/watch/Watch";
 
 const  App=()  =>{
   return (
-    <Home />
+    <Watch />
   )
 }
 
