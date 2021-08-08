20c231c42e1b7b17670da0f1b273a97fef2199a5
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 17:31:22 +1000

    feat: adding secure connection with dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const dotevnv = require("dotenv");
+
+dotevnv.config();
 
 mongoose
   .connect(process.env.MONGO_URL, {
