67af0fac63f16a6c18fb3871693f27b6a021c798
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 18:39:01 +1000

    feat: creating updating user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,32 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+
+//UPDATE
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+      ).toString();
+    }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can update only your account!");
+  }
+});
+
 module.exports = router;
