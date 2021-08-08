15b529b2e82c04a8a23de9a1dcb1a41e9e7d739f
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 17:26:50 +1000

    feat: connection with mongoos

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
 
 app.listen(8800, ()=> {
