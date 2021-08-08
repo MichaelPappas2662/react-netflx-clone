01fed2f80d382b4cf46973b993a6179058186e7a
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 18:01:04 +1000

    fix: making it async

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,15 +2,19 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 //REGISTER
-router.post("/register",(req,res) => {
+router.post("/register", async (req,res) => {
     const newUser = new User({
         username:req.body.username,
         email:req.body.email,
         password:req.body.password,
     });
 
-    const user = newUser.save();
-    res.json(user);
+    try {
+        const user = await newUser.save();
+        res.status(201).json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 module.exports = router;
