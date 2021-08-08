8da78acd048a0aaa18be37df504bb7a78067ca5b
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 18:15:51 +1000

    feat: debecript passwords

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -23,10 +23,23 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-    const user =
-  }catch(err){
-    res.status(500).json(err)
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username!");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
+
+    
+
+    
+
+    res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
   }
-})
+});
 
 module.exports = router;
