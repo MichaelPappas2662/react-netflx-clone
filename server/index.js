42102383666889be63fea1e012c47a9da5460fdf
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 19:47:41 +1000

    feat: rendering lists

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const dotevnv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotevnv.config();
 
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 
 app.listen(8800, ()=> {
