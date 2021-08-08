dd09796a986a12cb65465175a45252ac0008802d
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-08 18:43:51 +1000

    feat: creating get user stats

--- server/routes/users.js
+++ server/routes/users.js
@@ -72,4 +72,29 @@ router.get("/", verify, async (req, res) => {
   }
 });
 
+//GET USER STATS
+router.get("/stats", async (req, res) => {
+  const today = new Date();
+  const latYear = today.setFullYear(today.setFullYear() - 1);
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
