b0f54a45543fae2465935c7ceb5296ccedcadcb0
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-09 11:29:33 +1000

    feat: finalizing front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,46 @@
 import "./App.scss"
-import Home from './pages/home/Home';
+import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./authContext/AuthContext";
 
-const  App=()  =>{
+const App = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <Home />
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movie" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
 
