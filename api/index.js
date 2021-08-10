37c7f3154a1a48b45d7932759c67b2e3e1f61e2b
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-10 11:22:47 +1000

    feat: adding cors

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
 const listRoute = require("./routes/lists");
+const cors = require("cors");
 
 dotenv.config();
 
